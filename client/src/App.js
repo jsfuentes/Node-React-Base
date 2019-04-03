81b6042809fe50c7ca752aab7747a42d00831eda
Jorge Fuentes <jsjfuentesj@gmail.com>
2019-04-03 14:45:52 -0700

    Add express and react generator base

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+    fetch("/users")
+      .then(response => {console.log(response); return response.text();})
+      .then(data => console.log(data));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    console.log("HELP");
+    return (
+      <div>
+        <h1>Hello, worldssss!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
